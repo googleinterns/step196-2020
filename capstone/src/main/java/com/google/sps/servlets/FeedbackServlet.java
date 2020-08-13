2c338e5cd11a0477f528494fdd72d626f1f08a6a
Andrew Gan <agannct@gmail.com>
2020-08-13 21:26:08 +0000

    linter fixes

--- capstone/src/main/java/com/google/sps/servlets/FeedbackServlet.java
+++ capstone/src/main/java/com/google/sps/servlets/FeedbackServlet.java
@@ -48,10 +48,10 @@ public class FeedbackServlet extends HttpServlet {
     response.sendRedirect("/main.html");
   }
 
-   /*
-    * @return the request parameter, or the default value if the parameter was not specified by the
-    *     client
-    */
+  /*
+   * @return the request parameter, or the default value if the parameter was not specified by the
+   *     client
+   */
   private String getParameter(HttpServletRequest request, String paramName, String defaultValue) {
     String value = request.getParameter(paramName);
     if (value.length() == 0) {
