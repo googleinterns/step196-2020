7489cf6198a2eb32b757eb58ecfe5516435b2193
Andrew Gan <agannct@gmail.com>
2020-08-14 20:52:18 +0000

    Store place objects in datastore with associated tags and labels (black-owned, small-business)

--- capstone/src/main/java/com/google/sps/servlets/FeedbackServlet.java
+++ capstone/src/main/java/com/google/sps/servlets/FeedbackServlet.java
@@ -11,9 +11,9 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+ 
 package com.google.sps.servlets;
-
+ 
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
@@ -22,10 +22,10 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+ 
 @WebServlet("/feedback")
 public class FeedbackServlet extends HttpServlet {
-
+ 
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
     // Get the input from the form and add them to the database of suggestions in datastore
@@ -34,27 +34,29 @@ public class FeedbackServlet extends HttpServlet {
     String problemArea = getParameter(request, "problem-area-input", "");
     String suggestion = getParameter(request, "suggestion-input", "");
     String otherFeedback = getParameter(request, "feedback-input", "");
-
+ 
     Entity feedbackEntity = new Entity("Feedback");
     feedbackEntity.setProperty("name", name);
     feedbackEntity.setProperty("relation", relation);
     feedbackEntity.setProperty("problemArea", problemArea);
     feedbackEntity.setProperty("suggestion", suggestion);
     feedbackEntity.setProperty("otherFeedback", otherFeedback);
-
+ 
     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
     datastore.put(feedbackEntity);
-
+ 
     response.sendRedirect("/main.html");
   }
-
-
- /**
+ 
+  /*
    * @return the request parameter, or the default value if the parameter was not specified by the
    *     client
    */
   private String getParameter(HttpServletRequest request, String paramName, String defaultValue) {
     String value = request.getParameter(paramName);
-      return value || defaultValue;
+      if (value.length() == 0) {
+        return defaultValue;
+      }
+      return value;
   }
 }
--- capstone/src/main/java/com/google/sps/servlets/scraperServlet.java
+++ capstone/src/main/java/com/google/sps/servlets/scraperServlet.java
@@ -11,45 +11,78 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+ 
 package com.google.sps.servlets;
-
-import java.io.IOException;
+ 
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
 import com.google.gson.Gson;
+import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.ArrayList;
+import java.lang.String;
+import java.util.*;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-
+ 
 /** Servlet that returns some example content. TODO: modify this file to handle comments data */
 @WebServlet("/business-names")
 public class scraperServlet extends HttpServlet {
-
+ 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     ArrayList<String> restaurantNames = new ArrayList<>();
     String urlbase = "https://www.helpourneighborhoodrestaurants.com/";
     String[] locations = {"brooklyn", "manhattan", "queens", "staten-island", "bronx"};
-
+ 
     for (String location : locations) {
       String url = urlbase.concat(location);
       Document page = Jsoup.connect(url).userAgent("JSoup Scraper").get();
-
+ 
       String restaurantNameSelector = "div > h4 > a";
       Elements restaurantNameElements = page.select(restaurantNameSelector);
-
+ 
       for (Element restaurantName : restaurantNameElements) {
         restaurantNames.add(restaurantName.text());
       }
     }
-
+ 
     String restaurantNamesJson = new Gson().toJson(restaurantNames);
     response.setContentType("application/json;");
     response.getWriter().println(restaurantNamesJson);
+  }
+ 
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    String restaurantName = request.getParameter("businessName");
+    boolean blackOwned = false;
+    boolean smallBusiness = true;
+    // stores tags like "pizza" or "chinese"
+    // implemented as hashset for faster lookup
+    HashSet<String> tags = new HashSet<String>();
+    // because input is string regardless of actual type, we need to extract each value in list
+    // TODO: input type might change so will need to do specific character cuts
+    //      as needed e.g. if inputted as list must omit '[' and ']' chars when storing
+    String[] tagsAsStringValues = request.getParameter("tags").split(", ");
+    for (String tag : tagsAsStringValues) {
+      if (tags.contains(tag) == false) {
+        tags.add(tag);
+      }
+    }
+ 
+    Entity restaurantEntity = new Entity("Restaurant");
+    restaurantEntity.setProperty("name", restaurantName);
+    restaurantEntity.setProperty("blackOwned", blackOwned);
+    restaurantEntity.setProperty("smallBusiness", smallBusiness);
+    restaurantEntity.setProperty("tags", tags);
+ 
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    datastore.put(restaurantEntity);
+    response.sendRedirect("/login.html");
   }
 }
