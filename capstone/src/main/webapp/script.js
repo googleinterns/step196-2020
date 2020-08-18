ad185325a3366cdd8babf21b04aa7b93336052c3
Grace <graceehuu@gmail.com>
2020-08-18 16:38:58 +0000

    linter fixes- documentation

--- capstone/src/main/webapp/script.js
+++ capstone/src/main/webapp/script.js
@@ -24,7 +24,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
     and stored in _markersArray
  */
 let _map;
-let _markersArray = [];
+const _markersArray = [];
 let _center;
 let _showSmallBusiness = false;
 let _showBlackOwnedBusiness = false;
@@ -171,7 +171,9 @@ function getPlaceDetails(name, set) {
   });
 }
 
-/** Obtains search results from Places API */
+/** Obtains search results from Places API
+    @param {any} keyword search query keyword
+    @return top 20 places matching search results */
 function getPlacesSearchResults(keyword) {
   document.getElementById('map').style.width = '75%';
   document.getElementById('panel').style.display = 'block';
@@ -230,7 +232,7 @@ function setMarker(place) {
 
 /** Clears all markers on map */
 function clearMarkers() {
-  for (var i = 0; i < _markersArray.length; i++ ) {
+  for (let i = 0; i < _markersArray.length; i++ ) {
     _markersArray[i].setMap(null);
   }
   _markersArray.length = 0;
@@ -298,7 +300,7 @@ function closePanel() {
   document.getElementById('map').style.width = '100%';
 }
 
-/** Clears all exisiting markers on map 
+/** Clears all exisiting markers on map
     and gets filters from checked boxes, ie. small or black-owned */
 async function getInputFilters() {
   clearMarkers();
@@ -311,7 +313,7 @@ async function getInputFilters() {
     _showBlackOwnedBusiness = true;
   }
 
-  // if a filter is selected, do manual search, else do Nearby Search w Places API
+  // do manual search if filter is selected, else do Nearby Search w Places API
   if (_showSmallBusiness || _showBlackOwnedBusiness) {
     if (!isStringEmpty(keyword)) {
       const keywordEntities = getEntities(keyword);
@@ -323,7 +325,8 @@ async function getInputFilters() {
   document.getElementById('search-button').disabled = false;
 }
 
-/** checks if string is empty, contains only white space, or null */
+/** @param str input string to check
+    @return {boolean} if str is empty, contains only white space, or null */
 function isStringEmpty(str) {
   return (str.length === 0 || !str.trim() || !str);
 }
@@ -334,7 +337,10 @@ async function getReviewsEntities(reviews) {
   return reviewsEntities;
 }
 
-/** send POST request to Cloud Natural Language API for entity recognition */
+/** send POST request to Cloud Natural Language API for entity recognition 
+    @param messages message to passed into NLP API
+    @return Promise of entities from messages
+*/
 function getEntities(messages) {
   const url = '/nlp-entity-recognition?messages=' + messages;
   const requestParamPOST = {
@@ -343,7 +349,8 @@ function getEntities(messages) {
       'Content-Type': 'application/json',
     },
   };
-  return fetch(url, requestParamPOST).then((response) => response.json()).then((entities) => {
+  return fetch(url, requestParamPOST).then((response) => response.json())
+  .then((entities) => {
     return entities;
   }).catch((err) => {
     console.log('Error reading data ' + err);
