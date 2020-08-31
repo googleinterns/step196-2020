192daaefa0091764db8901ea49bb3973a1622692
Carolyn Asante-Dartey <cadartey@gmail.com>
2020-08-31 04:34:19 +0000

    Consistent formatting of server-side methods

--- capstone/src/main/java/com/google/sps/data/Restaurant.java
+++ capstone/src/main/java/com/google/sps/data/Restaurant.java
@@ -11,23 +11,27 @@ public class Restaurant {
   private final double rating;
   private final double lat;
   private final double lng;
-  private final String address;
-  private final String phone;
-  private final String price;
+  private final String formattedAddress;
+  private final String formattedPhoneNumber;
+  private final String priceLevel;
   private final String website;
-  private final String ID;
+  private final String placeId;
+  private final String vicinity;
+  private final boolean openStatus;
 
-  public Restaurant(String name, int numberOfReviews, double rating, double lat, double lng, String address, String phone, String price, String website, String ID) {
+  public Restaurant(String name, int numberOfReviews, double rating, double lat, double lng, String address, String phone, String price, String website, String ID, String vicinity) {
     this.name = name;
     this.numberOfReviews = numberOfReviews;
     this.rating = rating;
     this.lat = lat;
     this.lng = lng;
-    this.address = address;
-    this.phone = phone;
-    this.price = price;
+    this.formattedAddress = address;
+    this.formattedPhoneNumber = phone;
+    this.priceLevel = price;
     this.website = website;
     this.ID = ID;
+    this.vicinity = vicinity; 
+    this.openStatus = true;
   }
 
   public String name() {
